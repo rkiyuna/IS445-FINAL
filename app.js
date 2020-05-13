22c5e9d01c0a893543f4a99a42e97cbc41be71f7
Reece <rkeeyuna@gmail.com>
2020-05-12 20:58:00 -0700

    step 2: establish evenNum function

--- app.js
+++ app.js
@@ -2,17 +2,5 @@ function evenNum() {
   var num1 = parseInt(document.getElementById("num1").value);
   var num2 = parseInt(document.getElementById("num2").value);
 
-  // check if the numbers fit criteria
-  if (num1 <= 2 || num1 >= 100 || num2 <= 2 || num2 >= 100) {
-    document.getElementById("msg").innerHTML =
-      "Invalid input, please try again.";
-    document.getElementById("result").innerHTML = "";
-  } else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100) {
-    calculateEvenNum(num1, num2);
-  }
-}
-function calculateEvenNum(firstNum, secondNum) {
-  var eNumArray = [];
-  var sNum = 0;
-  var bNum = 0;
+  
 
