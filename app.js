47b7a34605649daa5cc50e9a807c2dd3deff7042
Reece <rkeeyuna@gmail.com>
2020-05-12 21:32:20 -0700

    step 3 fixed errors

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ function evenNum() {
   var num2 = parseInt(document.getElementById("num2").value);
 
   //check for valid numbers from user
-  if (num1 <= 2 || num1 >= 100) {
+  if (num1 < 2 || num1 >= 100) {
     console.log(parseInt(num1, 10));
     document.getElementById("msg").innerHTML +=
       "Number 1 input" + num1 + " is not a valid number.";
     document.getElementById("result").innerHTML = "";
   }
-  if (num2 <= 2 || num2 >= 100) {
+  if (num2 < 2 || num2 >= 100) {
     document.getElementById("msg").innerHTML +=
       "Number 1 input" + num2 + " is not a valid number.";
     document.getElementById("result").innerHTML = "";
