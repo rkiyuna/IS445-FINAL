047a59a3b4efdf00fa025c254924e6bd40945ff7
Reece <rkeeyuna@gmail.com>
2020-05-12 21:01:45 -0700

    step 2 print results, even functions

--- app.js
+++ app.js
@@ -3,41 +3,58 @@ function evenNum() {
   var num1 = parseInt(document.getElementById("num1").value);
   var num2 = parseInt(document.getElementById("num2").value);
 
-//check for valid numbers from user
-  if (num1 <= 2 || num1 >= 100){
-    console.log(parseInt(num1,10));
-    document.getElementById("msg").innerHTML += "Number 1 input" + num1 + " is not a valid number.";
+  //check for valid numbers from user
+  if (num1 <= 2 || num1 >= 100) {
+    console.log(parseInt(num1, 10));
+    document.getElementById("msg").innerHTML +=
+      "Number 1 input" + num1 + " is not a valid number.";
     document.getElementById("result").innerHTML = "";
-}
-if (num2 <= 2 || num2 >= 100){
-    document.getElementById("msg").innerHTML += "Number 1 input" + num2 + " is not a valid number.";
+  }
+  if (num2 <= 2 || num2 >= 100) {
+    document.getElementById("msg").innerHTML +=
+      "Number 1 input" + num2 + " is not a valid number.";
     document.getElementById("result").innerHTML = "";
+  } else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100) {
+    calculateEvenNum(num1, num2);
+  }
 }
-else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100){
-    calculateEvenNum(num1,num2);
-}
-}
-function calculateEvenNum(firstNum, secondNum){
-var eNumArray = [];
-var sNum = 0;
-var bNum = 0;
+//calculate even numbers
+function calculateEvenNum(firstNum, secondNum) {
+  var eNumArray = [];
+  var sNum = 0;
+  var bNum = 0;
 
-if (firstNum > secondNum){
+  if (firstNum > secondNum) {
     sNum = secondNum;
     bNum = firstNum;
-}
-else if (firstNum < secondNum){
+  } else if (firstNum < secondNum) {
     sNum = firstNum;
     bNum = secondNum;
-}
+  }
 
-while(sNum <= bNum){
-    if(isEven(sNum) == true){
-        eNumArray.push(sNum);
-        sNum = sNum + 1;
+  while (sNum <= bNum) {
+    if (isEven(sNum) == true) {
+      eNumArray.push(sNum);
+      sNum = sNum + 1;
+    } else if (isEven(sNum) == false) {
+      sNum = sNum + 1;
     }
-    else if (isEven(sNum) == false){
-        sNum = sNum + 1;
-    }
+  }
+  //print results
+  console.log(eNumArray);
+  document.getElementById("result").innerHTML =
+    "There are " + eNumArray.length + " even numbers:";
+  document.getElementById("msg").innerHTML = eNumArray;
 }
-
+//even function
+function isEven(num) {
+  res = true;
+  for (var i = num; i <= num + 1; i++) {
+    if (num % 2 == 0) {
+      res = true;
+    } else {
+      res = false;
+    }
+    return res;
+  }
+}
